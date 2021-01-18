bd85fc10ae20356fc2e99c7dd0798e593f9c1283
pedrojhrossi <pedrojhrossi@gmail.com>
2021-01-18 12:49:36 -0400

    Internationalization finished, Templates and Controllers

--- spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccessHandler.java
+++ spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -6,13 +6,17 @@
 package com.bolsadeideas.springboot.app.auth.handler;
 
 import java.io.IOException;
+import java.util.Locale;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 /**
@@ -21,16 +25,30 @@ import org.springframework.web.servlet.support.SessionFlashMapManager;
  */
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    
+    @Autowired
+    private MessageSource messageSource;
+    
+    @Autowired
+    private LocaleResolver localeResolver;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, 
             Authentication authentication) throws IOException, ServletException {
+        
         SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
+        
         FlashMap flashMap = new FlashMap();
-        flashMap.put("success", "Hola " + authentication.getName() + ", has iniciado sesion con exito!");
+        
+        Locale locale = localeResolver.resolveLocale(request);
+        String mensaje = String.format(messageSource.getMessage("text.login.success", null, locale), authentication.getName());
+        
+        flashMap.put("success", mensaje);
+        
         flashMapManager.saveOutputFlashMap(flashMap, request, response);
+        
         if (authentication != null) {
-            logger.info("El usuario '" + authentication.getName() + "', ha iniciado sesion con exito!");
+            logger.info(mensaje);
         }
         super.onAuthenticationSuccess(request, response, authentication); //To change body of generated methods, choose Tools | Templates.
     }
--- spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -55,8 +55,8 @@ public class ClienteController {
     private IUploadFileService uploadFileService;
     @Autowired
     private MessageSource messageSource;
-    
-    @Secured({"ROLE_USER","ROLE_ADMIN"}) //Arreglo para otorgar acceso a mas de un rol
+
+    @Secured({"ROLE_USER", "ROLE_ADMIN"}) //Arreglo para otorgar acceso a mas de un rol
     @RequestMapping(value = "/uploads/{filename:.+}", method = RequestMethod.GET)
     public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
         Resource recurso = null;
@@ -73,20 +73,22 @@ public class ClienteController {
 //    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_OTRO', 'ROLE_ADMIN')") //Arreglo para otorgar acceso a mas de un rol con hasAnyRole
 //    @Secured("ROLE_USER")
     @GetMapping(value = "/ver/{id}")
-    public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+    public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
         Cliente cliente = clienteService.fetchByIdWithFacturas(id); //clienteService.findOne(id);
         if (cliente == null) {
-            flash.addFlashAttribute("error", "El cliente no existe en la BBDD");
+            flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
             return "redirect: /listar";
         }
 
         model.put("cliente", cliente);
-        model.put("titulo", "Detalle cliente: " + cliente.getNombre());
+        model.put("titulo",
+                messageSource.getMessage("text.cliente.detalle.titulo", null, locale).concat(": ").concat(cliente.getNombre()));
         return "ver";
     }
 
     @RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
-    public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, Authentication authentication, HttpServletRequest request, Locale locale) {
+    public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+            Authentication authentication, HttpServletRequest request, Locale locale) {
         if (authentication != null) {
             logger.info("Hola usuario autenticado, tu username es: ".concat(authentication.getName()));
         }
@@ -128,41 +130,45 @@ public class ClienteController {
 
     @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/form")
-    public String crear(Map<String, Object> model) {
+    public String crear(Map<String, Object> model, Locale locale) {
 
         Cliente cliente = new Cliente();
         model.put("cliente", cliente);
-        model.put("titulo", "Formulario de Cliente");
+        model.put("titulo", messageSource.getMessage("text.cliente.form.titulo.crear", null, locale));
         return "form";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
 //    @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/form/{id}")
-    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
 
         Cliente cliente = null;
 
         if (id > 0) {
             cliente = clienteService.findOne(id);
             if (cliente == null) {
-                flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+                flash.addFlashAttribute("error",
+                        messageSource.getMessage("text.cliente.flash.db.error", null, locale));
                 return "redirect:/listar";
             }
         } else {
-            flash.addFlashAttribute("error", "El ID del cliente no puede ser 0!");
+            flash.addFlashAttribute("error",
+                    messageSource.getMessage("text.cliente.flash.id.error", null, locale));
             return "redirect:/listar";
         }
         model.put("cliente", cliente);
-        model.put("titulo", "Editar Cliente");
+        model.put("titulo", messageSource.getMessage("text.cliente.form.titulo.editar", null, locale));
         return "form";
     }
 
     @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, @RequestParam(name = "file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
+            @RequestParam(name = "file") MultipartFile foto, RedirectAttributes flash,
+            SessionStatus status, Locale locale) {
         if (result.hasErrors()) {
-            model.addAttribute("titulo", "Formulario de Cliente");
+            model.addAttribute("titulo", messageSource.getMessage("text.cliente.form.titulo", null, locale));
             return "form";
         }
 
@@ -179,12 +185,12 @@ public class ClienteController {
                 java.util.logging.Logger.getLogger(ClienteController.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            flash.addFlashAttribute("info", "Has subido correctamente '" + foto.getOriginalFilename() + "'");
+            flash.addFlashAttribute("info", messageSource.getMessage("text.cliente.flash.foto.subir.success", null, locale) + "'" + uniqueFilename + "'");
 
             cliente.setFoto(uniqueFilename);
         }
 
-        String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con exito!" : "Cliente creado con exito!";
+        String mensajeFlash = (cliente.getId() != null) ? messageSource.getMessage("text.cliente.flash.editar.success", null, locale) : messageSource.getMessage("text.cliente.flash.crear.success", null, locale);
         clienteService.save(cliente);
         status.setComplete();
         flash.addFlashAttribute("success", mensajeFlash);
@@ -193,16 +199,18 @@ public class ClienteController {
 
     @Secured("ROLE_ADMIN")
     @RequestMapping(value = "/eliminar/{id}")
-    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash, Locale locale) {
 
         if (id > 0) {
             Cliente cliente = clienteService.findOne(id);
 
             clienteService.delete(id);
-            flash.addFlashAttribute("success", "Cliente eliminado con exito!");
+            flash.addFlashAttribute("success",
+                    messageSource.getMessage("text.cliente.flash.eliminar.success", null, locale));
 
             if (uploadFileService.delete(cliente.getFoto())) {
-                flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con exito!");
+                String mensajeFotoEliminar = String.format(messageSource.getMessage("text.cliente.flash.foto.eliminar.success", null, locale), cliente.getFoto());
+                flash.addFlashAttribute("info", mensajeFotoEliminar);
             }
         }
         return "redirect:/listar";
