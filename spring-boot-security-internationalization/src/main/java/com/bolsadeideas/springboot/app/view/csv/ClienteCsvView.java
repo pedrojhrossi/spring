59a57a249d2cfc01d4d2479015c8728a739f8737
pedrojhrossi <pedrojhrossi@gmail.com>
2021-02-04 14:44:25 -0400

    CSV listar clientes finalizado

--- spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
+++ spring-boot-security-internationalization/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
@@ -5,16 +5,23 @@
  */
 package com.bolsadeideas.springboot.app.view.csv;
 
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import java.io.FileWriter;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 /**
  *
  * @author pj
  */
+//@Component("listar.csv") //TODO: Se utiliza para manejarlo y detectarlo por MIME Types en el application properties
 @Component("listar")
 public class ClienteCsvView extends AbstractView {
 
@@ -35,6 +42,19 @@ public class ClienteCsvView extends AbstractView {
         
         response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
         response.setContentType(getContentType());
+        Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+        
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.TAB_PREFERENCE);
+        
+        String[] header = {"id", "nombre", "apellido", "email", "createAt"};
+        
+        beanWriter.writeHeader(header);
+        
+        for(Cliente cliente : clientes) {
+            beanWriter.write(cliente, header);
+        }
+        
+        beanWriter.close();
     }
 
 }
