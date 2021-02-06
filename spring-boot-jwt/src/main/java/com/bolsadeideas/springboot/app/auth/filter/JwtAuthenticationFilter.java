946c850c222aa1173ca42c2519fd79dc0855de10
pedrojhrossi <pedrojhrossi@gmail.com>
2021-02-05 20:02:49 -0400

    Adding more data to the token

--- spring-boot-jwt/src/main/java/com/bolsadeideas/springboot/app/auth/filter/JwtAuthenticationFilter.java
+++ spring-boot-jwt/src/main/java/com/bolsadeideas/springboot/app/auth/filter/JwtAuthenticationFilter.java
@@ -5,12 +5,26 @@
  */
 package com.bolsadeideas.springboot.app.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -34,11 +48,10 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //        TODO: Por defecto los parametros que se manejan son username y password
 //        String username = obtainUsername(request); 
 //        String password = obtainPassword(request);
-
 //        TODO: Para parametros personalizados
         String username = request.getParameter("usuario");
         String password = request.getParameter("pass");
-        
+
         if (username == null) {
             username = "";
         }
@@ -58,7 +71,41 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 //        return super.attemptAuthentication(request, response); //To change body of generated methods, choose Tools | Templates.
         return authenticationManager.authenticate(authToken);
-
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        
+//        String username = authResult.getName();
+        String username = ((User) authResult.getPrincipal()).getUsername();
+        
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+        
+        Claims claims = Jwts.claims();
+        claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
+        
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512), SignatureAlgorithm.HS512)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + (3600000L*4L)))
+                .compact();
+        
+        response.addHeader("Authorization", "Bearer " + token);
+        
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("token", token);
+        body.put("user", (User) authResult.getPrincipal());
+        body.put("mensaje", String.format("Hola %s, Ha iniciado sesion con exito!", username));
+        
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
+        
+//        super.successfulAuthentication(request, response, chain, authResult); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 
 }
